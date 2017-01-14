a415b545382478855b9d852efcd621e880b32363
Benjamin <BenjaminDChambers@outlook.com>
2017-01-14 12:28:44 -0700

    Adding more internals

--- Connect4/Game.cs
+++ Connect4/Game.cs
@@ -9,17 +9,43 @@ namespace Connect4
     public class Game
     {
         #region Constructors
-        public Game(int nColumns, int nRows, int NeededToWin)
+        public Game(int nColumns, int nRows, int LengthNeededToWin)
         {
+            Width = nColumns;
+            Height = nRows;
+            NeededToWin = LengthNeededToWin;
             _current = new Board(nColumns, nRows);
-            _neededToWin = NeededToWin;
         }
         #endregion
 
         #region Actions
+        public void PlayMove(int Column)
+        {
+            if ((Column < 0) || (Column >= Width))
+                throw new ArgumentOutOfRangeException("Column");
+
+            _boardHistory.Add(new Board(_current));
+            _moveHistory.Add(Column);
+            _current.PutChecker(Column, WhoseMove);
+        }
         #endregion
 
         #region Info
+        public int Width
+        {
+            get;
+            private set;
+        }
+        public int Height
+        {
+            get;
+            private set;
+        }
+        public int NeededToWin
+        {
+            get;
+            private set;
+        }
         public Checker WhoseMove
         {
             get { return _moveHistory.Count % 2 == 0 ? Checker.Black : Checker.Red; }
@@ -33,7 +59,6 @@ namespace Connect4
         #endregion
 
         #region Internal
-        int _neededToWin;
         Board _current;
         List<int> _moveHistory = new List<int>();
         List<Board> _boardHistory = new List<Board>();
