a24218ca787667b586b07eaa8ed4ff99ebce682d
Benjamin <BenjaminDChambers@outlook.com>
2017-01-15 07:13:01 -0700

    Re-writing the history methods

---
+++ Connect4/HistoryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    public class HistoryItem
+    {
+        public HistoryItem(Board brd, GameState st, int move)
+        {
+            GameBoard = new Board(brd);
+            State = st;
+            SelectedMove = move;
+        }
+
+        public readonly Board GameBoard;
+        public readonly GameState State;
+        public readonly int SelectedMove;
+    }
+}
