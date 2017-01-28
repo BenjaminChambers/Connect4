2fb6e69f0c61f994c157f2696b8ff23659ff1bba
Benjamin <BenjaminDChambers@outlook.com>
2017-01-28 08:29:02 -0700

    Documented the Snapshot class

--- Connect4/Snapshot.cs
+++ Connect4/Snapshot.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Connect4
 {
+    /// <summary>
+    /// A more complete class for keeping track of board history.
+    /// It contains the board on that move, the state of the game at that point, and the move which resulted from this board
+    /// These snapshots are only meant to be created by the Game class, rather than consumers of the library.
+    /// </summary>
     public class Snapshot
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="brd">The game board</param>
+        /// <param name="st">The game state</param>
+        /// <param name="move">The move which resulted from this state</param>
         public Snapshot(Board brd, GameState st, int move)
         {
             GameBoard = new Board(brd);
@@ -15,10 +26,11 @@ namespace Connect4
             SelectedMove = move;
         }
 
-        // This is a snapshot of the current state of the board, and the move that resulted from it
-
+        /// <summary>The game board</summary>
         public readonly Board GameBoard;
+        /// <summary>The game state</summary>
         public readonly GameState State;
+        /// <summary>The move which resulted from this state</summary>
         public readonly int SelectedMove;
     }
 }
