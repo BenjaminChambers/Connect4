1a7e84c2be5333ca83c72f6b11591842fa9f07a4
Benjamin <BenjaminDChambers@outlook.com>
2017-01-28 10:18:07 -0700

    Added documentation to enums

--- Connect4/Enums.cs
+++ Connect4/Enums.cs
@@ -6,6 +6,29 @@ using System.Threading.Tasks;
 
 namespace Connect4
 {
-    public enum Checker { None, Black, Red }
-    public enum GameState { InProgress, BlackWins, RedWins, Tie }
+    /// <summary>
+    /// Represents a piece which has been placed on a <see cref="Board"/> 
+    /// </summary>
+    public enum Checker {
+        /// <summary>Represents an empty space</summary>
+        None,
+        /// <summary>Represents a black piece. Black has first move.</summary>
+        Black,
+        /// <summary>Represents a red piece. Red moves second.</summary>
+        Red
+    }
+
+    /// <summary>
+    /// Represents the current state of a <see cref="Game"/> 
+    /// </summary>
+    public enum GameState {
+        /// <summary>The game is currently being played</summary>
+        InProgress,
+        /// <summary></summary>
+        BlackWins,
+        /// <summary></summary>
+        RedWins,
+        /// <summary></summary>
+        Tie
+    }
 }
