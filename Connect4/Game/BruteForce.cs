dd221a0044d8dd01e19fc80c9cfa5b1e08b0c43e
Benjamin Chambers <benjamindchambers@outlook.com>
2017-09-05 11:22:24 -0600

    Added stubs for brute force recursion

---
+++ Connect4/Game/BruteForce.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Connect4
+{
+    public partial class Game
+    {
+        public List<(int Column, double Score)> BruteForceEvaluateMoves(int Depth)
+        {
+            // Black wins = 1, Red wins = -1
+            // It will be converted based on who's actually playing right now
+        }
+
+        private double BruteForceRecursion(Board work, int remainingDepth, Checker player)
+        {
+            switch(player)
+            {
+                case Checker.Black:
+                    var winning = 
+            }
+        }
+    }
+}
