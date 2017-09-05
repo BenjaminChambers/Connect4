e85e334f0f8ce07fe9a9f9eb230e47584e62700f
Benjamin Chambers <benjamindchambers@outlook.com>
2017-09-05 11:24:29 -0600

    Removed GetWinningMoves from Game, as it's in Board

--- Connect4/Game/Info.cs
+++ Connect4/Game/Info.cs
@@ -6,32 +6,6 @@ namespace Connect4
     public partial class Game
     {
         /// <summary>
-        /// Returns a list of winning moves for the specified player
-        /// </summary>
-        /// <param name="Player">Which player to check for</param>
-        /// <returns>a <see cref="List{Int}"/> </returns>
-        public List<int> GetWinningMoves(Checker Player)
-        {
-            var result = new List<int>();
-
-            for (int i = 0; i < Width; i++)
-            {
-                if (_current.ColumnHeight(i) < Height)
-                {
-                    var y = _current.ColumnHeight(i);
-                    int c1 = CountDir(i, y, 1, Player) + CountDir(i, y, 9, Player) + 1;
-                    int c4 = CountDir(i, y, 4, Player) + CountDir(i, y, 6, Player) + 1;
-                    int c7 = CountDir(i, y, 7, Player) + CountDir(i, y, 3, Player) + 1;
-                    int c2 = CountDir(i, y, 2, Player) + 1;
-
-                    if ((c1 >= NeededToWin) || (c4 >= NeededToWin) || (c7 >= NeededToWin) || (c2 >= NeededToWin))
-                        result.Add(i);
-                }
-            }
-            return result;
-        }
-
-        /// <summary>
         /// How many columns wide the board is
         /// </summary>
         public int Width
