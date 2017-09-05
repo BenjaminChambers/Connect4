cbd5fb71c82654388729f36adc5867f6b1817296
Benjamin Chambers <benjamindchambers@outlook.com>
2017-09-05 11:25:04 -0600

    Removed functions supporting GetWinningMoves

--- Connect4/Game/Game.cs
+++ Connect4/Game/Game.cs
@@ -28,33 +28,6 @@ namespace Connect4
         Board _current;
         List<Snapshot> _history = new List<Snapshot>();
 
-
-        static readonly int[] _dx = { 0, -1, 0, 1, -1, 0, 1, -1, 0, 1 };
-        static readonly int[] _dy = { 0, -1, -1, -1, 0, 0, 0, 1, 1, 1 };
-
-        Checker _boundsChecked(int Col, int Row)
-        {
-            if ((Col < 0) || (Col >= Width) || (Row < 0) || (Row >= Height))
-                return Checker.None;
-            return _current[Col, Row];
-        }
-
-        int CountDir(int Col, int Row, int Dir, Checker Who)
-        {
-            int targetX = Col + _dx[Dir];
-            int targetY = Row + _dy[Dir];
-
-            int count = 0;
-            while (_boundsChecked(targetX, targetY) == Who)
-            {
-                count++;
-                targetX += _dx[Dir];
-                targetY += _dy[Dir];
-            }
-
-            return count;
-        }
-
         static Random rnd = new Random();
         #endregion
     }
