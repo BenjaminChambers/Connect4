16abdefeeb4a40c7e3056e54253d8939603a25bf
Benjamin Chambers <benjamindchambers@outlook.com>
2017-09-14 11:22:39 -0600

    Added functions that count the number of checkers on the board as well as the longest run length for a given player

---
+++ Connect4/Board/Evaluate.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connect4
+{
+    public partial class Board : IEnumerable<Checker>
+    {
+        /// <summary>
+        /// Counts the number of checkers on the board
+        /// </summary>
+        /// <returns>A Tuple containing (<see cref="int"/>Black, <see cref="int"/>Red, <see cref="int"/>Empty)</returns>
+        public (int Black, int Red, int Empty) CountCheckers()
+        {
+            int b = 0, r = 0, n = 0;
+
+            foreach (var cell in _board)
+            {
+                switch (cell)
+                {
+                    case Checker.Black: b++; break;
+                    case Checker.Red: r++; break;
+                    default: n++; break;
+                }
+            }
+
+            return (b, r, n);
+        }
+
+        /// <summary>
+        /// Counts the longest run of checkers on the board, looking horizontally, vertically, and diagonally
+        /// </summary>
+        /// <param name="Player">The <see cref="Checker"/> color to verify</param>
+        /// <returns>The length of the run as an <see cref="int"/></returns>
+        public int LongestRun(Checker Player)
+        {
+            var h = CountHorizontal(Player);
+            var v = CountVertical(Player);
+
+            return Math.Max(h, v);
+        }
+
+        private int CountHorizontal(Checker Player)
+        {
+            int longest = 0;
+
+            for (int row = 0; row < Height; row++)
+            {
+                var current = 0;
+                for (int i = 0; i < Width; i++)
+                {
+                    if (_board[i, row] == Player)
+                        current++;
+                    else
+                    {
+                        if (current > longest)
+                            longest = current;
+                        current = 0;
+                    }
+                }
+            }
+
+            return longest;
+        }
+
+        int CountVertical(Checker Player)
+        {
+            var longest = 0;
+
+            for (int col = 0; col < Width; col++)
+            {
+                var current = 0;
+                for (int i = 0; i < _height[col]; i++)
+                {
+                    if (_board[col, i] == Player)
+                        current++;
+                    else
+                    {
+                        if (current > longest)
+                            longest = current;
+                        current = 0;
+                    }
+                }
+            }
+
+            return longest;
+        }
+
+        int CountDiagonalUp(Checker Player)
+        {
+            var longest = 0;
+
+            for (int row=Height-1; row>=0; row--)
+            {
+                var current = 0;
+
+                for (int i=0; i<Math.Min(Width, Height-row); i++)
+                {
+                    if (_board[i, row + i] == Player)
+                        current++;
+                    else
+                    {
+                        if (current > longest)
+                            longest = current;
+                        current = 0;
+                    }
+                }
+            }
+
+            for (int col=1; col<Width; col++)
+            {
+                var current = 0;
+
+                for (int i=0; i<Math.Min(Height, Width-col); i++)
+                {
+                    if (_board[col+i, i] == Player)
+                        current++;
+                    else
+                    {
+                        if (current > longest)
+                            longest = current;
+                        current = 0;
+                    }
+                }
+            }
+
+            return longest;
+        }
+
+        int CountDiagonalDown(Checker Player)
+        {
+            var longest = 0;
+
+            for (int row = 0; row<Height; row++)
+            {
+                var current = 0;
+
+                for (int i = 0; i < Math.Min(Width, row); i++)
+                {
+                    if (_board[i, row - i] == Player)
+                        current++;
+                    else
+                    {
+                        if (current > longest)
+                            longest = current;
+                        current = 0;
+                    }
+                }
+            }
+
+            for (int col = 1; col < Width; col++)
+            {
+                var current = 0;
+
+                for (int i = 0; i < Math.Min(Height, Width - col); i++)
+                {
+                    if (_board[col + i, Height-1-i] == Player)
+                        current++;
+                    else
+                    {
+                        if (current > longest)
+                            longest = current;
+                        current = 0;
+                    }
+                }
+            }
+
+            return longest;
+        }
+    }
+}
